82ec75799108bfb40a5ead569673188ed3fb72ff
Us <abuumarsharipov15@gmail.com>
2025-10-21 14:55:24 +0500

    fol

---
+++ task2.js
@@ -0,0 +1,60 @@
+//task1
+// function qq(arr){
+//     let sum=0
+//     for(let i of arr){
+//         sum+=i
+//     }
+//     return sum
+// }
+// console.log(qq([1,2,3,4,5]));
+
+
+//task2
+// function qq(arr){
+//     mx=-999
+//     for(let i of arr){
+//         if(i>mx){
+//             mx=i
+//         }
+//     }
+//     return mx
+// }
+// console.log(qq([1,2,4,5,534,23,3424]));
+
+
+//task3
+// function qq(arr){
+//     let even=0
+//     for(let i of arr){
+//         if(i%2==0){
+//             even++
+//         }
+//     }
+//     return even
+// }
+// console.log(qq([1,321,5322,234554,3254]));
+
+
+//task4
+// function qq(arr,a){
+//     for(let i of arr){
+//         if(i==a){
+//             return true
+//         }
+//     }
+//     return false
+// }
+// console.log(qq([1324,1234,12314,2,42,13,],42));
+
+
+//task5
+// function qq(arr){
+//     let sum=0
+//     let cnt=0
+//     for(let i of arr){
+//         sum+=i
+//         cnt++
+//     }
+//     return sum/cnt
+// }
+// console.log(qq([1,2,3,4,5,6,7,8,9]));
